47aae71e501e862f9ab8faf1c05e04a3d859b1d8
SagiMarton <sagi.marton1@gmail.com>
2023-09-20 18:59:40 +0200

    bit faster and more memory efficient

--- longest_substring/main.cpp
+++ longest_substring/main.cpp
@@ -5,40 +5,53 @@
 #include <utility>
 #include "header.hpp"
 
-int lengthOfLongestSubstring(std::string s){
-    std::unordered_map<char,int>* letters = new std::unordered_map<char,int>();
+int lengthOfLongestSubstring(std::string s)
+{
+    std::unordered_map<char, int> *letters = new std::unordered_map<char, int>();
     int length = 0;
     int maxlength = 0;
+    int first = 0;
     if (!s.empty())
     {
-        
+
         for (int i = 0; i < s.size(); i++)
-        {   
+        {
             if (letters->find(s[i]) == letters->end())
             {
-                letters->insert(std::make_pair(s[i],i));
+                letters->insert(std::make_pair(s[i], i));
                 length++;
                 if (length > maxlength)
                     maxlength = length;
             }
             else
             {
-                length = 0;
-                i = letters->at(s[i]);
-                letters->clear();
+                int where = letters->at(s[i]);
+                if (where < first)
+                {
+                    letters->erase(s[i]);
+                    letters->insert(std::make_pair(s[i], i));
+                    length++;
+                    if (length > maxlength)
+                        maxlength = length;
+                }
+                else
+                {
+                    first = where + 1;
+                    letters->erase(s[i]);
+                    letters->insert(std::make_pair(s[i], i));
+                    length = std::max(1, i + 1 - first);
+                }
             }
         }
-
     }
-    
 
     delete letters;
     return maxlength;
 }
 
-int main() {
-    std::cout << "asd" << std::endl;
+int main()
+{
+    std::string s = "dvdf";
+    std::cout << lengthOfLongestSubstring(s) << std::endl;
     return 0;
-
-
 }
